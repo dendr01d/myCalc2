b874eb6e9578b8e9de64f422a8e92678432dbd46
Duncan Miller <dfmi@umich.edu>
2016-03-18 04:22:34 -0400

    In the midst of debugging. Changed some bits in myCalc.c and sort.c

--- myCalc.c
+++ myCalc.c
@@ -5,18 +5,18 @@
 #include "pow.h"
 #include "sort.h"
 
-#define ADD 1
-#define SUB 2
-#define MUL 3
-#define DIV 4
-#define POW 5
-#define MOD 6
-#define SORT 7
+#define ADD 0
+#define SUB 1
+#define MUL 2
+#define DIV 3
+#define POW 4
+#define MOD 5
+#define SORT 6
 
 #define OPTIONS 7
 
 //operations can only be 4 letters long
-static char *options[][5] = { {"add"}, {"sub"}, {"mul"}, {"div"}, {"pow"}, {"mod"}, {"sort"}};
+static const char options[][5] = { {"add"}, {"sub"}, {"mul"}, {"div"}, {"pow"}, {"mod"}, {"sort"}};
 
 void printUsage() {
     printf("Usage: myCalc <operation> a b (c, d, e...)\n");
@@ -34,7 +34,7 @@ int whichOption(char *str){
 }
 
 int main (int argc, char *argv[]) {
-    if (argc < 4)) {
+    if (argc < 4) {
 	    printUsage();
     }
 
@@ -44,6 +44,8 @@ int main (int argc, char *argv[]) {
     #ifdef DEBUG
     printf("Just so you know, this is in debug mode!\n");
     #endif
+    
+    int array[argc - 2];
     
     switch(whichOption(argv[1])) {
         case ADD:
@@ -71,14 +73,18 @@ int main (int argc, char *argv[]) {
             break;
         
         case SORT:
-            int array[argc - 2];
-            for (int i = argc - 1; i > 1; --i) {
-                array[i] = atoi(argv[i]);
+            for (int i = 2; i < argc; ++i) {
+                array[i-2] = atoi(argv[i]);
             }
+            #ifdef DEBUG
+            printf("this is the array I'm working on...\n");
+            printList(array, argc - 2);
+            #endif
             radixSort(array, argc - 2);
             printList(array, argc - 2);
+            break;
         
-        case default:
+        default:
             printUsage();
             break;
     }
--- sort.c
+++ sort.c
@@ -1,68 +1,105 @@
-#include <climits>
-
-using namespace std;
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void printList(int array[], int length) {
     for (int j = 0; j < length; ++j) {
-        printf("%-3d", array[j];
+        printf("%-3d", array[j]);
+    }
+    printf("\n");
+}
+
+void printMaskedList(int array[], int length, int mask) {
+    for (int j = 0; j < length; ++j) {
+        printf("%-3d", array[j] & mask);
     }
     printf("\n");
 }
 
 
 void radixSort(int array[], int length) {
+    //here are some buckets, their (nominal) sizes, and an index holder
+    int* bucket0 = malloc(length * sizeof(bucket0));
+    int* bucket1 = malloc(length * sizeof(bucket1));
+    int size0 = 0;
+    int size1 = 0;
+    int ind0 = 0;
+    int ind1 = 0;
+    
+    int mask = 1;
+
     //for every digit in an int (32 of them)
-    for (int i = 1; i < INT_MAX; i *= 2) {
-        int numZ, numO;
+    for (int i = 0; i < 32; ++i) {
+        
         
         //use bit masking to see what the current digit is and count them
+        size0 = 0;
+        size1 = 0;
         for (int j = 0; j < length; ++j) {
-            if (array[j] & i) {
-                ++numO;
+            if ((array[j] & i) == 0) {
+                ++size0;
             }
             else {
-                ++numZ;
+                ++size1;
             }
         }
         
-        //create buckets to store the values
-        int *zeroes = new int[numZ];
-        int *ones = new int[numO];
-        
-        int *zP = zeroes;
-        int *oP = ones;
-        
         //sort the numbers into the buckets
+        ind0 = 0;
+        ind1 = 0;
         for (int j = 0; j < length; ++j) {
-            if (array[j] & i) {
-                &oP = array[j];
-                ++oP;
+            if ((array[j] & i) == 0) {
+                bucket0[ind1] = array[j];
+                ++ind0;
             }
             else {
-                &zP = array[j];
-                ++zP;
+                bucket1[ind1] = array[j];
+                ++ind1;
             }
         }
         
         //copy the numbers back into the original array
         
-        int *zP = zeroes;
-        int *oP = ones;
+        ind0 = 0;
+        ind1 = 0;
         int counter = 0;
         
-        for (int j = 0; j < numZ; ++j) {
-            array[counter] = &zP;
+        #ifdef DEBUG
+            printf("\n0 Bucket: ");
+        #endif
+        for (int j = 0; j < size0; ++j) {
+            array[counter] = bucket0[ind0];
+            #ifdef DEBUG
+                printf("%d ", bucket0[ind0]);
+            #endif
+            ++ind0;
             ++counter;
         }
-        for (int j = 0; j < numO; ++j) {
-            array[counter] = &zP;
+        #ifdef DEBUG
+            printf("\n1 Bucket: ");
+        #endif
+        for (int j = 0; j < size1; ++j) {
+            array[counter] = bucket1[ind1];
+            #ifdef DEBUG
+                printf("%d ", bucket1[ind1]);
+            #endif
+            ++ind1;
             ++counter;
         }
         
         //debug output
         #ifdef DEBUG
+            printf("\nMask: %d\n", mask);
             printList(array, length);
+            printMaskedList(array, length, mask);
         #endif
+        
+        //increment the mask
+        mask <<= 1;
     }
+        
+    //clear the buckets
+    free(bucket0);
+    free(bucket1);
 
 }
