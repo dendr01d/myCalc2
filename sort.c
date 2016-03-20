7c146b60bb5e1d7a167ff3bc3f59b6734e826411
Duncan Miller <dfmi@umich.edu>
2016-03-20 04:57:25 -0400

    Optimized the bucket counting/sorting and cleaned up the code a bit

--- sort.c
+++ sort.c
@@ -9,88 +9,51 @@ void printList(int array[], int length) {
     printf("\n");
 }
 
-void printMaskedList(int array[], int length, int mask) {
-    for (int j = 0; j < length; ++j) {
-        printf("%-3d", array[j] & mask);
-    }
-    printf("\n");
-}
-
-
 void radixSort(int array[], int length) {
     //here are some buckets, their (nominal) sizes, and an index holder
     int *bucket0 = malloc(length * sizeof(bucket0));
     int *bucket1 = malloc(length * sizeof(bucket1));
     int size0 = 0;
     int size1 = 0;
-    int ind0 = 0;
-    int ind1 = 0;
 
-    //for every digit in an int (the bitmask's bit will eventually be shifted off)
+    //for every digit in an int (the bitmask's bit will eventually be shifted off the end)
     for (unsigned int i = 1; i != 0; i <<= 1) {
         
-        
-        //use bit masking to see what the current digit is and count them
+        //use bit masking to count and sort each number into buckets
         size0 = 0;
         size1 = 0;
         for (int j = 0; j < length; ++j) {
             if ((array[j] & i) == 0) {
+                bucket0[j - size1] = array[j];
                 ++size0;
             }
             else {
+                bucket1[j - size0] = array[j];
                 ++size1;
             }
         }
         
-        //sort the numbers into the buckets
-        ind0 = 0;
-        ind1 = 0;
-        for (int j = 0; j < length; ++j) {
-            if ((array[j] & i) == 0) {
-                bucket0[ind0] = array[j];
-                ++ind0;
-            }
-            else {
-                bucket1[ind1] = array[j];
-                ++ind1;
-            }
-        }
-        
         //copy the numbers back into the original array
-        
-        ind0 = 0;
-        ind1 = 0;
         int counter = 0;
         
-        #ifdef DEBUG
-            printf("\nMask: %d\n", i);
-            printf("0 Bucket: ");
-        #endif
         for (int j = 0; j < size0; ++j) {
-            array[counter] = bucket0[ind0];
-            #ifdef DEBUG
-                printf("%d ", bucket0[ind0]);
-            #endif
-            ++ind0;
+            array[counter] = bucket0[j];
             ++counter;
         }
-        #ifdef DEBUG
-            printf("\n1 Bucket: ");
-        #endif
         for (int j = 0; j < size1; ++j) {
-            array[counter] = bucket1[ind1];
-            #ifdef DEBUG
-                printf("%d ", bucket1[ind1]);
-            #endif
-            ++ind1;
+            array[counter] = bucket1[j];
             ++counter;
         }
         
         //debug output
         #ifdef DEBUG
+            printf("\n\nMask: %d\n", i);
+            printf("0 Bucket: ");
+            printList(bucket0, size0);
+            printf("1 Bucket: ");
+            printList(bucket1, size1);
             printf("\n");
             printList(array, length);
-            //printMaskedList(array, length, i);
         #endif
     }
         
